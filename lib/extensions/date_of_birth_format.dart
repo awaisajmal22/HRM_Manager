3fb0deba8cb451c96b2e81ba36fb0f7e6cf4472d
awaisajmal22 <awaisajmal22@gmail.com>
2024-05-28 17:51:45 +0500

    "changes"

--- lib/extensions/date_of_birth_format.dart
+++ lib/extensions/date_of_birth_format.dart
@@ -1,15 +1,14 @@
 import 'package:intl/intl.dart';
 
-String dateFormater(String date){
+String dateFormater(String date) {
   print("Date is $date");
-  if (date.isEmpty || date == 'null' || date == null || date == ''){
+  if (date.isEmpty || date == 'null' || date == null || date == '') {
     return '';
-  }
-  else{
-  final year = DateFormat.y().format(DateTime.parse(date));
-  final month = DateFormat.LLL().format(DateTime.parse(date));
-  final day = DateFormat.d().format(DateTime.parse(date));
-  final dateTime = '$year, $month $day';
-  return dateTime;
+  } else {
+    final year = DateFormat.LLL().format(DateTime.parse(date));
+    final month = DateFormat.d().format(DateTime.parse(date));
+    final day = DateFormat.y().format(DateTime.parse(date));
+    final dateTime = '$year $month, $day';
+    return dateTime;
   }
 }
